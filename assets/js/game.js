d74c186b5dc52b9f6ca0879286d627188cdc8bf0
abbygraves <abbygraves14@gmail.com>
2022-05-20 01:26:42 -0500

    randomize attack order

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,7 +40,7 @@ var fightOrSkip = function() {
     }
   }
   return false;
-}
+};
 // ⚠️ ––––––––––––––––––– End of FIGHTorSKIP FUNCTION ––––––––––––––––––– ⚠️
 
 
@@ -48,52 +48,54 @@ var fightOrSkip = function() {
 
 // ⚠️ ––––––––––––––––––– FIGHT FUNCTION ––––––––––––––––––– ⚠️
 var fight = function(enemy) {
+  var isPlayerTurn = true;
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
   // ⬇︎ repeat and execute as long as enemy-robot is alive
   while(playerInfo.health > 0 && enemy.health > 0) {
-    if (fightOrSkip()) {
-      break;
+    if (isPlayerTurn) {
+      if (fightOrSkip()) {
+        break;
+      }
+      // ⬇︎ subtract playerInfo.attack variable from enemy.health
+      var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+      enemy.health = Math.max(0, enemy.health - damage);
+      console.log(
+        playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+      );
+
+      // ⬇︎ check enemy's health
+      if (enemy.health <= 0) {
+        window.alert(enemy.name + " has died!");
+
+        // award player money for winning
+        playerInfo.money = playerInfo.money + 20;
+        // leave while() since enemy is dead
+        break;
+      } else {
+        window.alert(enemy.name + " still has " + enemy.health + " health left.")
+      }
+    } else {
+
+      // ⬇︎ subtract enemy.attack variable from playerInfo.health
+      var damage = randomNumber(enemy.attack - 3, enemy.attack);
+      playerInfo.health = Math.max(0, playerInfo.health - damage);
+      console.log(
+        enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+      );
+
+      // ⬇︎ check player's health
+      if (playerInfo.health <= 0) {
+        window.alert(playerInfo.name + " has died!");
+        // ⬇︎ leave while loop if player is dead
+        break;
+      } else {
+        window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+      }
     }
-    // ⬇︎ subtract playerInfo.attack variable from enemy.health
-    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-    enemy.health = Math.max(0, enemy.health - damage);
-    console.log(
-      playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-    );
-
-    // ⬇︎ check enemy's health
-    if (enemy.health <= 0) {
-      window.alert(enemy.name + " has died!");
-
-      // award player money for winning
-      playerInfo.money = playerInfo.money + 20;
-      // leave while() since enemy is dead
-      break;
-    } 
-    else {
-      window.alert(enemy.name + " still has " + enemy.health + " health left.")
-    }
-
-    // ⬇︎ subtract enemy.attack variable from playerInfo.health
-    var damage = randomNumber(enemy.attack - 3, enemy.attack);
-    playerInfo.health = Math.max(0, playerInfo.health - damage);
-    console.log(
-      enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-    );
-
-    // ⬇︎ check player's health
-    if (playerInfo.health <= 0) {
-      window.alert(playerInfo.name + " has died!");
-      // ⬇︎ leave while loop if player is dead
-      break;
-    } 
-    else {
-      window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
-    }
-    
-    // ✕ DISABLED –– Invalid entry
-    //else {
-      //window.alert("You need to choose a valid option. Try again!");
-    
+    // switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;
   }
 }; // ⚠️ ––––––––––––––––––– END OF FIGHT FUNCTION ––––––––––––––––––– ⚠️
 
