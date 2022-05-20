a9db9893e1b7e032e77061a40633d936da8b8a44
abbygraves <abbygraves14@gmail.com>
2022-05-20 01:36:02 -0500

    highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -145,12 +145,21 @@ var startGame = function () {
 
 // ⚠️ ––––––––––––––––––– END GAME FUNCTION ––––––––––––––––––– ⚠️
 var endGame = function() {
-  // ⬇︎ if player still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + " 🎉");
+  window.alert("The game has now ended. Let's see how you did!");
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
   }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
   else {
-    window.alert("You've lost your robot in battle.");
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
   // ⬇︎ ask player if they'd like to play again
   var playerAgainConfirm = window.confirm("Would you like to play again?");
